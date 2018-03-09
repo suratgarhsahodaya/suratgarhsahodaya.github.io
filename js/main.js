87c43e5869ef816f0e78bfea37c0320244475edd
Himanshu Gupta <himanshugupta.396@gmail.com>
2018-03-09 19:37:02 +0530

    layout complete

--- js/main.js
+++ js/main.js
@@ -136,8 +136,8 @@ jQuery(document).ready(function( $ ) {
 
   // jQuery counterUp (used in Facts section)
   $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
+    delay: 5,
+    time: 500
   });
 
   // Porfolio isotope and filter
